8a7d194fb38513e2f5bfd7533cf8c0b06c2ed325
LazaroLia <lialazarojuarez@gmail.com>
2025-02-21 10:36:22 +0100

    Aparece la figura3

---
+++ tutorial-vue/src/App.vue
@@ -0,0 +1,27 @@
+<!-- App.vue -->
+<template>
+  <div id="app" class="container">
+  <div class="row">
+  <div class="col-md-12">
+  <h1>Personas</h1>
+  </div>
+  </div>
+  <div class="row">
+  <div class="col-md-12">
+  <tabla-personas />
+  </div>
+  </div>
+  </div>
+  </template>
+  <script setup>
+  import TablaPersonas from '@/components/TablaPersonas.vue'
+  defineOptions({
+  name: 'app',
+  });
+  </script>
+  <style>
+  button {
+  background: #009435;
+  border: 1px solid #009435;
+  }
+  </style>
---
+++ tutorial-vue/src/components/TablaPersonas.vue
@@ -0,0 +1,42 @@
+<!-- src/components/TablaPersonas.vue -->
+<template>
+    <div id="tabla-personas">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+       
+        <tbody>
+            <tr>
+                <td>Jon</td>
+                <td>Nieve</td>
+                <td>jon@email.com</td>
+            </tr>
+            <tr>
+        <td>Tyrion</td>
+        <td>Lannister</td>
+        <td>tyrion@email.com</td>
+        </tr>
+        <tr>
+        <td>Daenerys</td>
+        <td>Targaryen</td>
+        <td>daenerys@email.com</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+</template>
+
+
+
+<script setup>
+    defineOptions({name: 'tabla-personas',})
+</script>
+
+<style scoped>
+
+</style>
