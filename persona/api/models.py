294f5d79ca7fcebab1368130ca1d8dc3612f38e4
LazaroLia <lialazarojuarez@gmail.com>
2025-03-05 10:20:30 +0100

    fALTA MODELS.PY

--- persona/api/models.py
+++ persona/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    """Definición del modelo para representar un censo"""
+    id = models.CharField(max_length=9, primary_key=True)
+    nombre = models.CharField(max_length=128)
+    apellido = models.CharField(max_length=128)
+    email = models.EmailField()
+
+##HACER STR Y META
